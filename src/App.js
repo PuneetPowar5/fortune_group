84345d772d0f88aa1b6a58571338fef19d9dce8f
Orangeboy115 <puneetpowar10@gmail.com>
2021-07-21 13:36:19 -0400

    Added footer
    I added the footer file but still need to do the CSS for it to apper in the bottom of each page

--- src/App.js
+++ src/App.js
@@ -7,30 +7,36 @@ import Portfolio from './components/Portfolio';
 import Team from './components/Team';
 import About from './components/About';
 import Contact from './components/Contact';
+import Footer from './components/Footer';
 
 const App = () => {
   return (
     <Router>
       <Navbar />
-
       <Switch>
         <Route exact path='/'>
           <Home />
+          <Footer />
         </Route>
         <Route path='/team'>
           <Team />
+          <Footer />
         </Route>
         <Route path='/portfolio'>
           <Portfolio />
+          <Footer />
         </Route>
         <Route path='/stocks'>
           <Stocks />
+          <Footer />
         </Route>
         <Route path='/about'>
           <About />
+          <Footer />
         </Route>
         <Route path='/contact'>
           <Contact />
+          <Footer />
         </Route>
       </Switch>
     </Router>
