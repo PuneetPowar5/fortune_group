3c439c4bf3be4aa08c4abbdc0a09babb478d5d8d
Orangeboy115 <puneetpowar10@gmail.com>
2021-08-06 13:33:07 -0400

    Added all content for the homepage without the CSS

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,8 +7,20 @@ const Home = () => {
             <div className='CompName'>
                 THE FORTUNE GROUP
             </div>
-            <div className='Slogan'>
-                LEARNING TO TRADE TOGETHER
+            <div className='Intro'>
+                The Fortune Group, established by a team of 10 university students aims to maximize capital gains through value investing philosophy
+            </div>
+            <div className='Quote'>
+                "The intelligent investor is a realist who sells to optimists and buys from pessimists" - Benjamin Graham
+            </div>
+            <div className='Goals'>
+                Our 3 Goals:
+                    Capital Gains:
+                        Maximizing profits with thorough analysis of stocks
+                    Acquire Useful Skills:
+                        Understand and Interpret business news while building financial models
+                    Network:
+                        Connect with like-minded individuals who are finance/engineer oriented 
             </div>
         </div>
     );
