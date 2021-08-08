9f337f84118a4475cabef4b83e51e851a02342bd
Orangeboy115 <puneetpowar10@gmail.com>
2021-08-08 02:36:44 -0400

    Got the background image to fit perfectly and the quote to move down under the image

--- src/components/Quote.js
+++ src/components/Quote.js
@@ -4,8 +4,15 @@ import './Quote.css';
 const Quote = () => {
     return(
         <div className='QuotePage'>
-            <div className='Quote'>
-                "The intelligent investor is a realist who sells to optimists and buys from pessimists" - Benjamin Graham
+            <div className='Container'>
+                <div className='Col'>
+                    <div className='TextWrapper'>
+                        <div className='Quote'>
+                            "The intelligent investor is a realist who sells to optimists and buys from pessimists" - Benjamin Graham
+                            </div>
+                    </div>
+                </div>
+
             </div>
         </div>
     );
