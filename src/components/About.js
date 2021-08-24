5bdfff1299b2b9d9d929847e8c9a2f3ad764e775
muhammad-zaidi123 <smazaidi2003@gmail.com>
2021-08-24 14:05:35 -0400

    added about us page

--- src/components/About.js
+++ src/components/About.js
@@ -1,13 +1,32 @@
 import React from 'react';
+import './About.css';
 
 const About = () => {
-    return(
-        <div>
-            <h2>
-                About Us
-            </h2>
-        </div>
-    );
-}
+  return (
+    <div className='background'>
+      <div className='about'>
+        <h2>About Us</h2>
+        <div className='separator'></div>
+        <div className='space'></div>
+        <ul>
+          <h2>
+            The Fortune Group is an investment management company, encompassing
+            investments pooled by a group of investors. The firm’s primary
+            mandate is to focus on capital gains by following a strategic value
+            investing strategy, targeting undervalued stocks classified by
+            investing fundamental characteristics. The Fortune Group is made
+            possible with weekly analyst meetings encompassing presentations of
+            stock reports. By actively managing $5,000+ diversified funds within
+            the Canadian equities market, a value investing philosophy is
+            utilized to optimize learning and portfolio growth.
+          </h2>
+          <h2>
+            “Risk comes from not knowing what you are doing” - Warren Buffet
+          </h2>
+        </ul>
+      </div>
+    </div>
+  );
+};
 
-export default About
+export default About;
--- src/components/Muhammed.js
+++ src/components/Muhammed.js
@@ -3,21 +3,19 @@ import './Muhammed.css';
 import muhammed from './muhammed.jpg';
 
 const Muhammed = () => {
-    return(
-        <div className='MuhammedPage'>
-            <div className='Container'>
-                <div className='Muhammed'>
-                    Muhammed Zaidi Software Engineer
-                </div>
-                <div className='MuhammedDescription'>
-                    Utilizes React JS to create a web development for the company
-                </div>
-                <div>
-                    <img src={muhammed} alt='Muhammed' className='MuhammedImage'/>
-                </div>
-            </div>
+  return (
+    <div className='MuhammedPage'>
+      <div className='Container'>
+        <div className='Muhammed'>Muhammad Zaidi Software Engineer</div>
+        <div className='MuhammedDescription'>
+          Utilizes React JS to create a web development for the company
         </div>
-    );
-}
+        <div>
+          <img src={muhammed} alt='Muhammed' className='MuhammedImage' />
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Muhammed
+export default Muhammed;
