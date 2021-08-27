2cffa4d0adb70f0fea1f40513d24d7cf4e7b343e
Bluesuey <42977553+Bluesuey@users.noreply.github.com>
2021-08-27 13:18:34 -0400

    Update some stuff

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,30 +1,33 @@
 import React from 'react';
-import linkedIn from '../pics/linkedIn.jpg';
-import instagram from '../pics/Instagram.png';
-import './Contact.css';
+import linkedIn from "../pics/linkedIn.jpg";
+import instagram from "../pics/Instagram.png";
+import "./Contact.css";
 
 const Contact = () => {
-  return (
-    <div className='background'>
-      <div className='separator'></div>
+    return(
+        <div className="background">
+            <div className="separator"></div>
+            
+            <div className="contacts">
+            <h2>
+                Contact Us
+            </h2>
+                <h4>If you would like to get in contact or would like more information, please contact us via one of our social media below:</h4>   
+                <>
+                <a href="https://www.linkedin.com/company/thefortunegroup/" target="_blank" rel="noopener noreferrer">
+                    <h4>LinkedIn:</h4>
+                    <img src={linkedIn} alt="linkedIn"/>
+                
+                </a>
+                </>
+                <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ" target="_blank" rel="noopener noreferrer">
+                    <h4>Instagram:</h4>
+                    <img src={instagram} alt="instagram"/>
+                </a>
+            </div>
+            
+        </div>
+    );
+}
 
-      <div className='contacts'>
-        <h2>Contact Us</h2>
-        <h4>
-          If you would like to get in contact or would like more information,
-          please contact us via one of our social media below:
-        </h4>
-        <a href='https://www.linkedin.com/company/thefortunegroup/'>
-          <h4>LinkedIn:</h4>
-          <img src={linkedIn} alt='linkedIn' />
-        </a>
-        <a href='https://www.youtube.com/watch?v=dQw4w9WgXcQ'>
-          <h4>Instagram:</h4>
-          <img src={instagram} alt='instagram' />
-        </a>
-      </div>
-    </div>
-  );
-};
-
-export default Contact;
+export default Contact
