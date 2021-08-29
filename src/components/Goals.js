cc9316fc5081383c0367f87ace124897cc31b0b9
Orangeboy115 <puneetpowar10@gmail.com>
2021-08-29 02:18:14 -0400

    Started making the values page on the homepage

--- src/components/Goals.js
+++ src/components/Goals.js
@@ -1,13 +1,45 @@
 import React from 'react';
 import './Goals.css';
+import {MdAttachMoney} from 'react-icons/md';
+import {SiSkillshare} from 'react-icons/si';
+import {GiShakingHands} from 'react-icons/gi';
 
 const Goals = () => {
     return(
         <div className='GoalsPage'>
             <div className='Container'>
                             <div className='Goals'>
-                                Our 3 Goals
+                                Our Values
                             </div>
+                            <ul className='Values'>
+                                <li className='Capital'>
+                                    <MdAttachMoney className='CapitalIcon'></MdAttachMoney>
+                                    <div className='CapitalText'>
+                                        Capital Gains
+                                    </div>
+                                    <div>
+                                        Description
+                                    </div>
+                                </li>
+                                <li>
+                                    <SiSkillshare></SiSkillshare>
+                                    <div>
+                                        Acquire Useful Skills
+                                    </div>
+                                    <div>
+                                        Description
+                                    </div>
+                                </li>
+                                <li>
+                                    <GiShakingHands></GiShakingHands>
+                                    <div>
+                                        Network
+                                    </div>
+                                    <div>
+                                        Description
+                                    </div>
+                                </li>
+                            </ul>
             </div>
         </div>
     );
