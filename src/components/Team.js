572da151a82e3a645929748bef64fb378c6517b4
Bluesuey <42977553+Bluesuey@users.noreply.github.com>
2021-08-29 16:58:14 -0400

    Delete team page
    (sorry puneet)

--- src/components/Team.js
+++ src/components/Team.js
@@ -1,46 +1,10 @@
 import React from 'react';
-import Andrew from './Andrew';
-import Brady from './Brady';
 import Footer from './Footer';
-import Karnik from './Karnik';
-import Muhammed from './Muhammed';
-import Puneet from './Puneet';
-import Rhys from './Rhys';
-import Sajeev from './Sajeev';
-import Steve from './Steve';
-import Trevor from './Trevor';
-import Will from './Will';
+
 
 const Team = () => {
     return(
         <div>
-            <div>
-                <Steve />
-            </div>
-            <div>
-                <Will />
-            </div>
-            <div>
-                <Sajeev />
-            </div>
-            <div>
-                <Trevor />
-            </div>
-            <div>
-                <Karnik />
-            </div>
-            <div>
-                <Puneet />
-            </div>
-            <div>
-                <Rhys />
-            </div>
-            <div>
-                <Muhammed />
-            </div>
-            <div>
-                <Andrew />
-            </div>
             <div>
                 <Footer />
             </div>
