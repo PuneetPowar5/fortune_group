a1362d1657b5d1521a5da9d4729ff6170d274d62
muhammad-zaidi123 <smazaidi2003@gmail.com>
2021-09-02 14:01:30 -0400

    added footer to stock pitches

--- src/components/Stocks.js
+++ src/components/Stocks.js
@@ -1,51 +1,79 @@
 import React from 'react';
-import "./Stocks.css"
-import airCanada from "../stock_pitches/air_canada.pdf";
-import cineplex from "../stock_pitches/cineplex.pdf"
-import doman from "../stock_pitches/doman_building_materials_group.pdf"
-import enbridge from "../stock_pitches/enbridge.pdf"
-import td from "../stock_pitches/td_bank_stock_pitch.pdf"
-import wsp from "../stock_pitches/wsp_global_inc.pdf"
-import stock from "../pics/buyside-stock-pitch-header.png"
-import Slideshow from "./Slideshow.js"
+import './Stocks.css';
+import airCanada from '../stock_pitches/air_canada.pdf';
+import cineplex from '../stock_pitches/cineplex.pdf';
+import doman from '../stock_pitches/doman_building_materials_group.pdf';
+import enbridge from '../stock_pitches/enbridge.pdf';
+import td from '../stock_pitches/td_bank_stock_pitch.pdf';
+import wsp from '../stock_pitches/wsp_global_inc.pdf';
+import stock from '../pics/buyside-stock-pitch-header.png';
+import Slideshow from './Slideshow.js';
 import Navbar from './Navbar/Navbar';
+import Footer from './Footer';
 
 const Stocks = () => {
-    return(
-        <>
-        <div>
-            <Navbar />
+  return (
+    <>
+      <div>
+        <Navbar />
+      </div>
+
+      <div className='stocksBody'>
+        <h2 className='header'>Stock Pitches</h2>
+        <img className='stocks' src={stock} alt='stock'></img>
+        <div className='buy'>
+          <p className='title'>Buy Decisions</p>
+          <p className='pitch'>
+            <a href={airCanada} target='blank'>
+              Air Canada (TSX: AC)
+            </a>
+          </p>
+          <p className='pitch'>
+            <a href={cineplex} target='blank'>
+              Cineplex (TSX: CGX)
+            </a>
+          </p>
+          <p className='pitch'>
+            <a href={doman} target='blank'>
+              Doman Building Materials Group (TSX: DBM)
+            </a>
+          </p>
+          <p className='pitch'>
+            <a href={td} target='blank'>
+              Toronto-Dominion Bank (TSX: TD)
+            </a>
+          </p>
+          <p className='pitch'>
+            <a href={wsp} target='blank'>
+              WSP Global (TSX: WSP)
+            </a>
+          </p>
+          <p className='pitch'>
+            <a href={wsp} target='blank'>
+              Intact Financial Corporation (TSX: IFC)
+            </a>
+          </p>
+          <p className='pitch'>
+            <a href={wsp} target='blank'>
+              Alimentation Couche-Tard Inc (TSX: ATD.B)
+            </a>
+          </p>
         </div>
-
-        <div className="stocksBody">
-            <h2 className="header">
-                Stock Pitches
-            </h2>
-            <img className="stocks" src={stock} alt="stock"></img>
-            <div className="buy">
-                <p className="title">Buy Decisions</p>
-                <p className="pitch"><a href={airCanada} target="blank">Air Canada (TSX: AC)</a></p>
-                <p className="pitch"><a href={cineplex} target="blank">Cineplex (TSX: CGX)</a></p>
-                <p className="pitch"><a href={doman} target="blank">Doman Building Materials Group (TSX: DBM)</a></p>
-                <p className="pitch"><a href={td} target="blank">Toronto-Dominion Bank (TSX: TD)</a></p>
-                <p className="pitch"><a href={wsp} target="blank">WSP Global (TSX: WSP)</a></p>
-                <p className="pitch"><a href={wsp} target="blank">Intact Financial Corporation (TSX: IFC)</a></p>
-                <p className="pitch"><a href={wsp} target="blank">Alimentation Couche-Tard Inc (TSX: ATD.B)</a></p>
-            </div>
-            <div className="hold">
-                <p className="title">Hold Decisions</p>
-                <p className="pitch"><a href={enbridge} target="blank">Enbridge (TSX: ENB)</a></p>
-                
-                
-            </div>
-            
+        <div className='hold'>
+          <p className='title'>Hold Decisions</p>
+          <p className='pitch'>
+            <a href={enbridge} target='blank'>
+              Enbridge (TSX: ENB)
+            </a>
+          </p>
         </div>
-        </>
-    );
-}
+      </div>
+      <Footer />
+    </>
+  );
+};
 
-export default Stocks
-
+export default Stocks;
 
 /*
 <div className="presentations">
